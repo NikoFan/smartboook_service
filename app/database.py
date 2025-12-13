eee9e537596746060a59245e269624a3e90ef12e
NikoFan <neoleg2005@gmail.com>
2025-12-13 21:27:27 +0300

    add check-if-module to db connect process

--- app/database.py
+++ app/database.py
@@ -5,7 +5,11 @@ from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = os.getenv('DATABASE_URL')
 
-if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+if not DATABASE_URL:
+    raise RuntimeError("❌ DATABASE_URL is not set! Check Render environment variables.")
+
+# Исправление для Render (postgres:// → postgresql://)
+if DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(
