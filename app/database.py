961560d6fa3b7f4a40f8e8d546cd2dd9d7888853
NikoFan <neoleg2005@gmail.com>
2025-12-22 02:47:44 +0300

    changed database.py file; added pool for RENDER

--- app/database.py
+++ app/database.py
@@ -2,6 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import QueuePool
 
 DATABASE_URL = os.getenv('DATABASE_URL')
 
@@ -14,9 +15,11 @@ if DATABASE_URL.startswith("postgres://"):
 
 engine = create_engine(
     DATABASE_URL,
-    pool_size=1,
-    max_overflow=0,
-    pool_pre_ping=True  # переподключается, если соединение "мертво"
+    poolclass=QueuePool,
+    pool_size=1,          # только 1 активное подключение
+    max_overflow=0,       # не пытаться создавать дополнительные
+    pool_pre_ping=True,   # проверяет подключение перед использованием (полезно при таймаутах)
+    pool_recycle=300      # пересоздаёт подключение каждые 5 минут (на случай таймаута idle)
 )
 
 
